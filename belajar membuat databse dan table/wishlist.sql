2825b76b27cb5c93e6cfc98964efa301a24c7d40
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-26 20:53:29 +0700

    Belajar Join

--- belajar membuat databse dan table/wishlist.sql
+++ belajar membuat databse dan table/wishlist.sql
@@ -34,3 +34,39 @@ SELECT * FROM wishlist;
 SELECT * FROM product;
 INSERT INTO wishlist(id_product,description)
 values ('pxxx', 'makanan Kesukaan');
+
+SELECT * FROM wishlist JOIN product p on p.id = wishlist.id_product;
+
+SELECT
+    w.id,
+    p.id,
+    p.name,
+    w.description
+FROM wishlist w
+    join product p on w.id_product = p.id;
+
+desc customers;
+
+ALTER TABLE  wishlist
+ADD COLUMN  id_customer INT;
+
+ALTER TABLE wishlist
+ADD CONSTRAINT fk_wishlist_customer
+FOREIGN KEY (id_customer) REFERENCES  customers(id);
+
+SELECT  * FROM  customers;
+
+UPDATE wishlist
+SET wishlist.id_customer= 1 WHERE wishlist.id =1;
+
+SELECT
+    w.id,
+    c.id,
+    p.id,
+    c.email,
+    p.id,
+    p.name,
+    w.description
+FROM wishlist w
+    join product p on w.id_product = p.id
+    JOIN customers c ON w.id_customer = c.id;
